da39bc268e3fe94af348690262fc116e3e0b2c9c
Leo Trubach <trubach@tnsintec.kz>
2015-06-24 21:35:45 +0600

    Fix deprecated modules for content types

--- attachments/admin.py
+++ attachments/admin.py
@@ -1,7 +1,7 @@
 from attachments.models import Attachment
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes import admin
 
 
-class AttachmentInlines(generic.GenericStackedInline):
+class AttachmentInlines(admin.GenericStackedInline):
     model = Attachment
     extra = 1
