6aee3867f256ae0f5505191b58e6e48ab2feeccd
Leo Trubach <leo@berkut-tech.kz>
2015-11-06 16:30:13 +0600

    Fix: deprecation

--- attachments/views.py
+++ attachments/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.http import HttpResponseRedirect
-from django.db.models.loading import get_model
+from django.apps.apps import get_model
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext, ugettext_lazy as _
 from django.template.context import RequestContext
