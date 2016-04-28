0f653f0000ebdaaa825afc5cf1aa17612bd4bcac
Leo Trubach <leo@berkut-tech.kz>
2016-04-28 16:14:04 +0600

    Fix: deprecation

--- attachments/models.py
+++ attachments/models.py
@@ -6,6 +6,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import fields
 from django.utils.translation import ugettext_lazy as _
 
+
 class AttachmentManager(models.Manager):
     def attachments_for_object(self, obj):
         object_type = ContentType.objects.get_for_model(obj)
