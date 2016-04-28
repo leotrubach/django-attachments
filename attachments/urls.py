67b116f4191e14b656d4d4e2946beb336e6e7d64
Leo Trubach <leo@berkut-tech.kz>
2016-04-28 16:24:54 +0600

    Fix: typo

--- attachments/urls.py
+++ attachments/urls.py
@@ -4,6 +4,6 @@ from attachments.views import add_attachment, delete_attachment
 urlpatterns = patterns('',
     url(r'^add-for/(?P<app_label>[\w\-]+)/(?P<module_name>[\w\-]+)/(?P<pk>\d+)/$',
         add_attachment, name="add_attachment"),
-    url(r'^delete/(?P<attachment_pk>\d+)/$', 'attachments.views.delete_attachment',
+    url(r'^delete/(?P<attachment_pk>\d+)/$',
         delete_attachment, name="delete_attachment"),
 )
